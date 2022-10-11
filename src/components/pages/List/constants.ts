25ceb57b635119f8050f416a6d70523724b23b1a
DIMASHPK <shupimonstr@gmail.com>
2022-10-11 16:12:10 +0300

    5_filters_implementation: moved list constants to list folder

---
+++ src/components/pages/List/constants.ts
@@ -0,0 +1,43 @@
+export const TOOLS_FORM_VALUES_KEYS = {
+  SORT: 'sorting',
+  FILTER: 'filters',
+  FILTER_CONDITION: 'filtersCondition',
+} as const;
+
+export const FILTERS_CONDITION_OPTIONS = {
+  AND: {
+    label: 'And',
+    value: 'and',
+  },
+  OR: {
+    label: 'Or',
+    value: 'or',
+  },
+};
+
+export const FILTER_NAMES = {
+  NAME: 'name',
+  SIZE: 'size',
+  DATE: 'date',
+  DESCRIPTION: 'description',
+} as const;
+
+export const FILTERS = {
+  [FILTER_NAMES.NAME]: {
+    NAME: FILTER_NAMES.NAME,
+  },
+  [FILTER_NAMES.SIZE]: {
+    NAME: FILTER_NAMES.SIZE,
+  },
+  [FILTER_NAMES.DATE]: {
+    NAME: FILTER_NAMES.DATE,
+  },
+  [FILTER_NAMES.DESCRIPTION]: {
+    NAME: FILTER_NAMES.DESCRIPTION,
+  },
+};
+
+export const SORT_OPTIONS = {
+  DESCENDING: 'descending',
+  ASCENDING: 'ascending',
+};
